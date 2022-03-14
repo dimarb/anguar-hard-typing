98989dc670a0f584888d2ba590aaed97ea66f2bb
Dimar Borda <dimar@arkdia.co>
2022-03-14 14:57:31 -0500

    first commit

--- src/app/models/person-class.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { PersonClass } from './person-class.model';
-
-describe('PersonClass', () => {
-  it('should create an instance', () => {
-    expect(new PersonClass()).toBeTruthy();
-  });
-});
